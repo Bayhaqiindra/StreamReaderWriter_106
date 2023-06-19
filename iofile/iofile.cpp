69cc1b282bb53c40b5e5dc7127e4dcb269f290f9
bayhaqiindra <bayhaqi30072004@gmail.com>
2023-06-19 11:34:59 +0700

    menunjuk ke sebuah file

--- iofile/iofile.cpp
+++ iofile/iofile.cpp
@@ -28,3 +28,7 @@ int main() {
 
 	//membuka file dalam mode membaca
 	ifstream infile;
+	//menunjuk ke sebuah file
+	infile.open("contohfile.txt");
+
+	cout << endl << ">= Membuka dan membaca file " << endl;
