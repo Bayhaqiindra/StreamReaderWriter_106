c5ffdc61a0a5b25e9e454c91941cffd618de8b5b
bayhaqiindra <bayhaqi30072004@gmail.com>
2023-06-19 11:37:58 +0700

    menunjuk ke sebuah nama file

--- iofileWithPath/iofileWithPath.cpp
+++ iofileWithPath/iofileWithPath.cpp
@@ -12,3 +12,8 @@ int main() {
 
 	// membukka file dalam mode menulis
 	ofstream outfile;
+	// menunjuk ke sebuah nama file
+	outfile.open(NamaFile, ios::out);
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
