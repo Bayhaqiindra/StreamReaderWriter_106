0c2aff7298a5b3c47bf25f0cd0684c8736ba30e3
bayhaqiindra <bayhaqi30072004@gmail.com>
2023-06-19 11:42:04 +0700

    jika selain integer maka blok ini akan diesekusi

--- manualException/manualException.cpp
+++ manualException/manualException.cpp
@@ -12,3 +12,10 @@ int main()
 		//blok ini akan dieksekusi
 		cout << "Pengecualian akan dieksekusi" << endl;
 	}
+	catch (...) {
+		/*jika selain integer maka blok ini akan diesekusi*/
+		cout << "Default Pengecualian dieksekusi" << endl;
+	}
+
+	return 0;
+}
