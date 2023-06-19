6270b3f1f86b83f8b3f0cb92246c282c28b42c6b
bayhaqiindra <bayhaqi30072004@gmail.com>
2023-06-19 11:43:39 +0700

    pesan array integer 3 elemen,memanggil array elemen ke 5

--- exceptionStandar/exceptionStandar.cpp
+++ exceptionStandar/exceptionStandar.cpp
@@ -9,3 +9,8 @@ int main()
 {
 	cout << "Awal Program" << endl; //penanda 1:....
 	try {
+		array<int, 3> data = { 1, 2, 3 };
+		//pesan array integer 3 elemen
+		//cout << data.at (5) << endl;
+		//memanggil array elemen ke 5
+	}
